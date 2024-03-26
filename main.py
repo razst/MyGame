06f72bec043103fce548453f63ad222d9545b907
Raz <raz_st@hotmail.com>
2024-03-26 11:33:42 +0200

    added car

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 from pygame.locals import *
 
+MOVE_SPEED = 50
+
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((562,600))
 
@@ -9,20 +11,37 @@ bg = pygame.image.load("bg.jpg")
 car = pygame.image.load("car.png")
 
 
-# car_x = screen.get_width()/2 - car.get_size()(0)/2
+car_x = screen.get_width()/2 + 10
+car_y = screen.get_height() - car.get_size()[1]
 
-# screen.blit(ship, (ship_left,ship_top))
 
 
-pygame.display.set_caption('car racer')
+def check_boundries():
+    global car_x
+    if car_x<0:
+        car_x=0
+    if car_x>screen.get_width()-car.get_size()[0]:
+        car_x = screen.get_width()-car.get_size()[0]
+
+
+
+
+pygame.display.set_caption('Car racer')
 
 while True:
     clock.tick(60)
-
-    screen.blit(bg,(0,0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                car_x -= MOVE_SPEED
+            elif event.key == K_RIGHT:
+                car_x += MOVE_SPEED
+    
+    check_boundries()
+    screen.blit(bg,(0,0))
+    screen.blit(car, (car_x,car_y))
+
 
     pygame.display.update()
